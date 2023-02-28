a50b7ba39676c3c7b9bb7b3e0893bff7875dd36e
AbrahaMD1010 <amiguel@unal.edu.co>
2023-02-27 22:16:37 -0500

    Update control.py

--- televisores/control.py
+++ televisores/control.py
@@ -12,19 +12,19 @@ class Control():
         self.tv = tv
 
     def turnOn(self):
-        self.tv.turnOn
+        self.tv.turnOn()
     def turnOff(self):
-        self.tv.turnOff
+        self.tv.turnOff()
     
     def canalUp(self):
         self.tv.canalUp
     def canalDown(self):
-        self.tv.canalDown
+        self.tv.canalDown()
 
     def volumenUp(self):
         self.tv.volumenUp
     def volumenDown(self):
-        self.tv.volumenDown
+        self.tv.volumenDown()
     
     def setCanal(self):
-        self.tv.setCanal
+        self.tv.setCanal()
